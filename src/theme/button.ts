c86896126f09f22a6334e85652b2f650182e684a
cadi <dbtmdgns4296@naver.com>
2024-07-04 12:28:33 +0900

    custom button theme & use responsive theme

---
+++ src/theme/button.ts
@@ -0,0 +1,36 @@
+import { ComponentStyleConfig } from '@chakra-ui/react';
+
+export const ButtonStyle: ComponentStyleConfig = {
+  baseStyle: {
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    borderRadius: 'base',
+  },
+  sizes: {
+    sm: {
+      fontSize: 'sm',
+      px: [2, 3, 4], // responsive values
+      py: [1, 2, 3], // responsive values
+    },
+    md: {
+      fontSize: 'md',
+      px: [3, 4, 5], // responsive values
+      py: [2, 3, 4], // responsive values
+    },
+  },
+  variants: {
+    outline: {
+      border: '2px solid',
+      borderColor: ['green.100', 'yellow.200', 'orange.300', 'red.400'],
+      color: ['green.100', 'yellow.200', 'orange.300', 'red.400'],
+    },
+    solid: {
+      bg: 'purple.500',
+      color: 'white',
+    },
+  },
+  defaultProps: {
+    size: 'md',
+    variant: 'outline',
+  },
+};
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,9 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
 import { fonts } from 'app/fonts/fonts';
+import { breakpoints } from '@theme/breakpoints';
+import { ButtonStyle } from '@theme/button';
 
 export const theme = extendTheme({
   fonts: {
     heading: fonts.bungee.style.fontFamily,
     body: fonts.bungee.style.fontFamily,
   },
+  components: {
+    Button: ButtonStyle,
+  },
+  breakpoints: breakpoints,
 });
