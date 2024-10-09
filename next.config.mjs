6305de032f892d940d675c3de33775fb2f7512a5
cadi <dbtmdgns4296@naver.com>
2024-10-09 15:53:39 +0900

    change webpack details

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 import createMDX from '@next/mdx';
+import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   output: 'standalone',
+  webpack: (config, { isServer }) => {
+    config.resolve.alias['~'] = path.join('./src', '/');
+    return config;
+  },
 };
 
 const withMDX = createMDX({
