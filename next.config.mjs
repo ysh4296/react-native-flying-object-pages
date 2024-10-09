947e787c13ddd2e82912bdfc652d2667a7e312f9
cadi <dbtmdgns4296@naver.com>
2024-10-09 16:00:39 +0900

    add webpack alias setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 import createMDX from '@next/mdx';
 import path from 'path';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   output: 'standalone',
   webpack: (config, { isServer }) => {
-    config.resolve.alias['~'] = path.join('./src', '/');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
   },
 };
