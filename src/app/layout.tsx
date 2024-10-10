4d66a42e687a2227418a6b13d64f18e0b11ade4c
cadi <dbtmdgns4296@naver.com>
2024-10-10 21:18:56 +0900

    optimizing seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import { Box } from '@chakra-ui/react';
 import Header from '@component/Header';
+import { Metadata } from 'next';
 import { Providers } from './providers';
+
+export const metadata: Metadata = {
+  title: 'react native flying objects',
+  description: 'React Native Package for Implement Interactive Objects',
+  openGraph: {
+    url: 'https://react-native-flying-objects.com',
+    title: 'react native flying objects',
+    description: 'React Native Package for Implement Interactive Objects',
+    images: [
+      {
+        url: 'https://react-native-flying-objects.com/favicon.ico',
+        width: 800,
+        height: 600,
+        alt: 'Og Image Alt',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
