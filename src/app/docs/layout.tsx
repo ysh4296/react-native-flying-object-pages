48e2a1fdf893adc3be9dd464c0bdd742f39de7fd
cadi <dbtmdgns4296@naver.com>
2024-10-10 19:54:12 +0900

    add code box padding

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <HStack h="100vh" pt={16} align="start">
       <DocsList hideBelow="md" />
-      <Box px={16} width="100%" height="100%" overflowY="auto">
+      <Box px={[1, 8, 16]} width="100%" height="100%" overflowY="auto">
         {children}
       </Box>
     </HStack>
